8538f5b36b83068d927a1d3606b602f8a4c0c529
Xiaoxia Yang <z5474541@ad.unsw.edu.au>
2026-01-05 18:18:36 +0800

    Revert "Build feature description and product description section"
    This reverts commit 68b395874c245e8775fced40e0dd52c7b51ffac6.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import FeatureDescription from "@/components/featureDescription";
 import Hero from "@/components/hero";
-import ProductDescription from "@/components/productDescription";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <FeatureDescription />
-      <ProductDescription />
-    </>
-  );
+  return <Hero />;
 }
